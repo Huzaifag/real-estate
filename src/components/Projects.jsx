5793745384032d372a611cccc59fcaa3610d9eeb
Huzaifa Gulzar <huzaifa6715@gmail.com>
2025-02-15 21:21:41 +0500

    add Testimonials component and integrate it into the App

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -5,29 +5,29 @@ const Projects = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [cardToShow, setCardToShow] = useState(1);
 
-  useEffect(()=>{
+  useEffect(() => {
     const updateCardToShow = () => {
-      if(window.innerWidth >= 1024){
+      if (window.innerWidth >= 1024) {
         setCardToShow(projectsData.length);
-    }else{
-      setCardToShow(1);
-    }
-  }
-  updateCardToShow();
-  window.addEventListener('resize', updateCardToShow);
+      } else {
+        setCardToShow(1);
+      }
+    };
+    updateCardToShow();
+    window.addEventListener('resize', updateCardToShow);
 
-  return () => {
-    window.removeEventListener('resize', updateCardToShow);
-  }
-},[]);
-  
+    return () => {
+      window.removeEventListener('resize', updateCardToShow);
+    };
+  }, []);
 
-  const nextProject = () =>{
+  const nextProject = () => {
     setCurrentIndex((prevIndex) => (prevIndex + 1) % projectsData.length);
-  }
-  const prevProject = () =>{
-    setCurrentIndex((prevIndex) => prevIndex ===0 ? projectsData.length - 1 : prevIndex - 1);
-  }
+  };
+  const prevProject = () => {
+    setCurrentIndex((prevIndex) => (prevIndex === 0 ? projectsData.length - 1 : prevIndex - 1));
+  };
+  
   return (
     <div className='container mx-auto py-4 pt-20 px-6 md:px-20 lg:px-32 my-20 w-full overflow-hidden' id='project'>
       <h1 className='text-2xl sm:text-4xl font-bold mb-2 text-center'>Projects <span className='underline underline-offset-4 decoration-1 under font-light'>Completed</span></h1>
@@ -45,15 +45,15 @@ const Projects = () => {
 
       {/* projects slide container  */}
       <div className='overflow-hidden'>
-        <div className='flex gap-8 transition-transform ease-in-out duration-500' style={{ transform: `translateX(-${(currentIndex * 100)/cardToShow}%)` }}>
-          {projectsData.map((project, index)=>(
+        <div className='flex gap-8 transition-transform ease-in-out duration-500' style={{ transform: `translateX(-${(currentIndex * 100) / cardToShow}%)` }}>
+          {projectsData.map((project, index) => (
             <div key={index} className='relative flex-shrink-0 sm:w-1/4'>
               <img src={project.image} alt={project.title} className='w-full h-auto mb-14' />
               <div className='absolute left-0 right-0 bottom-5 flex justify-center'>
                 <div className='inline-block bg-white w-3/4 px-4 py-2 shadow-md'>
                   <h2 className='text-xl font-semibold text-gray-800'>{project.title}</h2>
                   <p className='text-gray-500 text-sm'>
-                    {project.price} <span>|</span> {project.location}
+                    {project.price} <span className='px-1'>|</span> {project.location}
                   </p>
                 </div>
               </div>
@@ -62,7 +62,7 @@ const Projects = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
